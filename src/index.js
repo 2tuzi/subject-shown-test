bde4bddbde60c6a4a0eefdff6744944af195c4c1
sunxiaozheng <sunxiaozheng@gaosiedu.com>
2018-11-06 17:11:23 +0800

    feat(subject-select): add select

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import Cat from './cat.js';
-import Dog from './dog.js';
-export { Dog, Cat };
+import { TPL_SELECT, TPL_SELECT_OPTION} from './template/question-select';
+
+function showQuestionSelect(data) {
+    const optionKeys = ['A', 'B', 'C', 'D', 'E', 'F'];
+    const { topic, options } = data;
+    const optionsHtmlArr = options.map((item, index)=> {
+        let html = TPL_SELECT_OPTION.replace('{{key}}', optionKeys[index]);
+
+        return html.replace('{{option}}', item.describe);
+    });
+    const optionsHtml = optionsHtmlArr.join('');
+
+    return TPL_SELECT.replace('{{topic}}', topic).replace('{{options}}', optionsHtml);
+}
+
+function showQuestionFillBlank() {
+
+}
+
+function showQuestion(type = '1', data = {}) {
+    let html;
+
+    switch (type) {
+        case '1':
+            html = showQuestionSelect(data);
+            break;
+        case '2':
+            html = showQuestionFillBlank(data);
+            break;
+        default :
+            html = '<div>none match err!</div>';
+            break;
+
+    };
+    return html;
+}
+
+export default showQuestion;
---
+++ src/template/question-select.js
@@ -0,0 +1,10 @@
+export const TPL_SELECT = `
+    <div class='subject-wrapper'>
+        <div class='topic'>{{topic}}<div>
+        <div class='options'>{{options}}<div>
+    </div>
+`;
+
+export const TPL_SELECT_OPTION = `
+    <div class='option'>{{key}}. {{option}}<div>
+`;
